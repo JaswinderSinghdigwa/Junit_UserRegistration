d5282b07f051ff70145070555c23869cccc157cf
Aman <you@example.com>
2021-10-05 06:56:35 +0530

    validation of last name

--- src/main/sources/com/bl/regex/Registration.java
+++ src/main/sources/com/bl/regex/Registration.java
@@ -7,31 +7,19 @@ import java.util.regex.Pattern;
 public class Registration {
     public static final Scanner scanner = new Scanner(System.in);
 
-    public void validName(){
-        System.out.print("Enter Your First Name : ");
-        String firstname = scanner.next();
+    public boolean validName(String firstname){
         String regex = "^[A-Z]{1}[a-z]{2,10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstname);
-        if (matcher.matches())
-            System.out.println("First name is in pattern");
-        else
-            System.out.println("First name is not in pattern");
+        return matcher.matches();
     }
     
 
-    public static void validLastName(){
-        System.out.println("Enter the Last Name");
-        String lastname = scanner.next();
-
-        String regex1 = "^[A-Za-z]{1}[a-z]{3,10}$";
-
+    public boolean validLastName(String lastname){
+        String regex1 = "^[A-Z]{1}[a-z]{3,10}$";
         Pattern pattern1 = Pattern.compile(regex1);
         Matcher matcher1 = pattern1.matcher(lastname);
-        if (matcher1.matches())
-            System.out.println("Last name is in pattern");
-        else
-            System.out.println("Last name is in pattern");
+        return (matcher1.matches());
     }
 }
 
