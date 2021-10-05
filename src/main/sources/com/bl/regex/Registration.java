4b7bcac91ba323af9b111bfd8d9327c72876bbfa
Aman <you@example.com>
2021-10-05 07:58:22 +0530

    validation of passwordform4

--- src/main/sources/com/bl/regex/Registration.java
+++ src/main/sources/com/bl/regex/Registration.java
@@ -4,113 +4,65 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Registration {
     public static final Scanner scanner = new Scanner(System.in);
 
-    public void validName(){
-        System.out.print("Enter Your First Name : ");
-        String firstname = scanner.next();
+    public boolean validName(String firstname){
         String regex = "^[A-Z]{1}[a-z]{2,10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstname);
-        if (matcher.matches())
-            System.out.println("First name is in pattern");
-        else
-            System.out.println("First name is not in pattern");
+        return matcher.matches();
     }
     
 
-    public void validLastName(){
-        System.out.println("Enter the Last Name");
-        String lastname = scanner.next();
-
+    public boolean validLastName(String lastname){
         String regex1 = "^[A-Za-z]{1}[a-z]{3,10}$";
-
         Pattern pattern1 = Pattern.compile(regex1);
         Matcher matcher1 = pattern1.matcher(lastname);
-        if (matcher1.matches())
-            System.out.println("Last name is in pattern");
-        else
-            System.out.println("Last name is in pattern");
+        return matcher1.matches();
     }
     
-    public void validEmail(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Email Address : ");
-        String email = scanner.next();
+    public boolean validEmail(String email){
         String regex2 = "^A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
         Pattern pattern2 = Pattern.compile(regex2);
         Matcher matcher2 = pattern2.matcher(email);
-        if (matcher2.matches())
-            System.out.println("Email Address is found");
-        else
-            System.out.println("Email Address is not found");
+        return matcher2.matches();
     }
     
-    public static void validPhoneNumber(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your mobile number : ");
-        String mobileno = scanner.nextLine();
+    public boolean validPhoneNumber(String mobileno){
         String regex3 = "^(0/91)?[7-9][0-9]{10}$";
         Pattern pattern3 = Pattern.compile(regex3);
         Matcher matcher3 = pattern3.matcher(mobileno);
-        if (matcher3.matches())
-            System.out.println("Phone number is Valid");
-        else
-            System.out.println("Phone number is Invalid");
+        return matcher3.matches();
     }
     
-    public static void validPasswordRule1(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Password according to rule 1 : ");
-        String password = scanner.nextLine();
+    public boolean validPasswordRule1(String password){
         String regex4 = "^[A-Z a-z 0-9]{8,}$";
         Pattern pattern4 = Pattern.compile(regex4);
         Matcher matcher4 = pattern4.matcher(password);
-        if (matcher4.matches())
-            System.out.println("Password is Valid");
-        else
-            System.out.println("Password is Invalid");
+        return matcher4.matches();
     }
     
-    public static void validPasswordRule2(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Password according to rule 2 : ");
-        String password1 = scanner.nextLine();
+    public boolean validPasswordRule2(String password1){
         String regex5 = "^[A-Z]{1}+[a-zA-z0-9]{8,}$";
         Pattern pattern5 = Pattern.compile(regex5);
         Matcher matcher5 = pattern5.matcher(password1);
-        if (matcher5.matches())
-            System.out.println("Password is Valid");
-        else
-            System.out.println("Password is Invalid");
+        return (matcher5.matches());
     }
     
-    public static void validPasswordRule3(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the PassWord");
-        String passWord2 = scanner.nextLine();
+    public boolean validPasswordRule3(String password2){
         String regex6 = "^[A-Z]{1}+[0-9].{8,}$";
         Pattern pattern6 = Pattern.compile(regex6);
-        Matcher matcher6 = pattern6.matcher(passWord2);
-        if (matcher6.matches())
-            System.out.println("Password is Valid");
-        else
-            System.out.println("Password is Invalid");
+        Matcher matcher6 = pattern6.matcher(password2);
+        return matcher6.matches();
     }
     
-    public  void validPasswordRule4(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the PassWord : ");
-        String passWord2 = scanner.next();
+    public  boolean validPasswordRule4(String password3){
         String regex6 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\\\S+$).{8, 20}$";
         Pattern pattern6 = Pattern.compile(regex6);
-        Matcher matcher6 = pattern6.matcher(passWord2);
-        if (matcher6.matches()	)
-            System.out.println("Password is Valid");
-        else
-            System.out.println("Password is Invalid");
+        Matcher matcher6 = pattern6.matcher(password3);
+        return matcher6.matches();
     }
+    
 }
-
-
---
+++ src/test/sources/UserRegistration.java
@@ -0,0 +1,93 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.bl.regex.Registration;
+
+public class UserRegistration {
+	Registration registration = new Registration();
+	
+	@Test
+    public void Firstname_Shouldbe_True_whenitisproper() {
+     Assert.assertTrue(registration.validName("Roney"));
+    }
+	//
+	public void Firstname_Shouldbe_False_whenitisnotproper() {
+	     Assert.assertFalse(registration.validName("Roney"));
+
+	}
+	
+	@Test
+    public void Lastname_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validLastName("Singh"));
+
+    }
+
+	public void Lastname_Shouldbe_False_whenitisnotproper() {
+	     Assert.assertFalse(registration.validLastName("singh"));
+
+	}
+	
+	@Test
+	public void Email_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validEmail("abc.111@gmail.com"));
+
+	}
+	
+	public void Email_Shouldbe_false_whenitisnotproper() {
+	     Assert.assertFalse(registration.validEmail("abc2!!!;@gmail.com"));
+
+	}
+	
+	@Test
+	public void Mobileno_Shouldbe_True_whenitisproper1() {
+        Assert.assertTrue(registration.validPhoneNumber("919666983674"));
+
+	}
+	
+	public void Mobileno_Shouldbe_false_whenitisnotproper() {
+        Assert.assertFalse(registration.validPhoneNumber("91966698367421"));
+
+	}
+	
+	@Test
+	public void Passwordtypeone_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validPasswordRule1("12245@344"));
+
+	}
+
+	public void Passwordtypeone_Shouldbe_False_whenitisproper() {
+        Assert.assertFalse(registration.validPasswordRule1("12"));
+
+	}
+	
+	@Test
+	public void Passwordtypetwo_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validPasswordRule2("Password@12"));
+
+	}
+	
+	public void Passwordtypetwo_Shouldbe_False_whenitisnotproper() {
+        Assert.assertFalse(registration.validPasswordRule2("12"));
+	}
+	
+	@Test
+	public void Passwordtypethree_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validPasswordRule3("123passW@12"));
+
+	}
+	
+	public void Passwordtypethree_Shouldbe_False_whenitisnotproper() {
+        Assert.assertFalse(registration.validPasswordRule3("12"));
+	}
+	
+	@Test
+	public void Passwordtypefour_Shouldbe_True_whenitisproper() {
+        Assert.assertTrue(registration.validPasswordRule3("123passW@12."));
+
+	}
+
+	public void Passwordtypefour_Shouldbe_False_whenitisnotproper() {
+        Assert.assertFalse(registration.validPasswordRule3("12"));
+	}
+
+}
